1dec5c63449079a03f9f327343755b9739082522
Pradeep Singh <pdeepsingh91@gmail.com>
2017-10-14 21:06:25 -0700

    Update arraycode.c

--- assign2/arraycode.c
+++ assign2/arraycode.c
@@ -1,33 +1,3 @@
-Last login: Thu Sep 21 13:14:58 on ttys000
-Pradeeps-MacBook-Pro:~ pradeepsingh$ ssh -l singh ophelia2
-singh@ophelia2's password: 
-Last login: Thu Sep 21 13:12:58 2017 from 146.244.35.37
-[singh@ophelia2 ~]$ cd assing2
--bash: cd: assing2: No such file or directory
-[singh@ophelia2 ~]$ ls
-assign1  assign2
-[singh@ophelia2 ~]$ cd assign2
-[singh@ophelia2 assign2]$ ls
-arraycode.c  maxvals  maxvals.c
-[singh@ophelia2 assign2]$ vi arraycode.c
-
-
-
-
-
-
-
-
-
-
-
-
-/* arraycode.c -- J. Otto, 9/6/17 -- a helper code to be
-   compiled along with a separate main() program files. this
-   file provides to main() pointers to external arrays along
-   with an integer variable containing the size of the arrays.
-   -- see /examples/week3/maxval2.c.                           */
-
 /* this OBJECT module can be compiled (along with
    maxval2) code by doing:
 
