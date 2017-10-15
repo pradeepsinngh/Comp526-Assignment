324f57c241ad902ede2309bd5f4d3fb2a45a5d13
Pradeep Singh <pdeepsingh91@gmail.com>
2017-10-14 23:01:47 -0700

    Create goldbach.c

---
+++ Goldbach Conjecture/goldbach.c
@@ -0,0 +1,80 @@
+/*
+ * File Name: goldbach.c
+ * Author: Pradeep Singh
+ * Date: 10/14/2017
+ * Language: C
+ * Purpose: To check Goldbach Conjecture for a list of positive integers.
+ * Description:
+ 
+ * Program usage is as:
+ 
+ */
+
+/* Include following header files */
+
+#include <stdio.h>      // for printf() and scanf()
+
+/* Function declaration */
+ 
+_Bool isodd(int M);                                             //
+
+_Bool isprime(int M);                                           //
+
+void isGoldbach(int M, int *passfail, int *prime1, int *prime2); //
+
+void printResults(int N, int integers[N], int reason[N][3]);     //
+
+/* Main() starts here */
+
+int main(){
+  ftpr = fopen();
+  
+  /* make end != EOF, and initialize sum to zero... */
+  float fnum, fsum=0;
+  int end=EOF+1, icount;
+  float avg;
+
+  printf("let's add some numbers. Please\n");
+  printf("enter some numbers, CTL-D to quit:\n\n");
+
+/* remember to say "address of" with scanf() */
+
+  icount = 0;
+  end = scanf("%f", &fnum);
+  while (end != EOF) {
+    printf("running sum: %f+%f=", fsum, fnum);
+    fsum += fnum;
+    icount++;
+    printf("%f\n", fsum);
+    end = scanf("%f", &fnum);
+  }
+
+  fclose (ftpr);
+  work(N);
+  return;
+}
+
+void work (int N){
+int integers[N];
+int reason[N][3];
+
+ftpr = fopen("integers", );
+if (!ftpr) return;
+
+for (ii=0; ii<N; ii++){
+  fscanf(ftpr,"%d", &intergers[ii]);
+  
+  isGoldbach (integers[ii], &reason[ii][0], &reason[ii][1], &reason[ii][2]);
+  
+  }
+  
+ printResults(N,intergers,reasons);
+ 
+ return;
+ }
+
+
+
+
+
+
