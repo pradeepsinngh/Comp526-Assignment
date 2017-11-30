dd81d92ef9ef03e24d61c9705be5fe8740996060
Pradeep Singh <pdeepsingh91@gmail.com>
2017-11-30 08:54:09 -0800

    Update cpp.pl

--- cpp.pl
+++ cpp.pl
@@ -2,11 +2,11 @@
 # File name: cpp.pl
 # Date: 11/30/2017
 # Author: Pradeep Singh
-# description: The program "cpp.pl" functions as a compiler pre-processor (CPP) in the follopwing ways:
-             it scans a text file indicated on the command line for special lines like the following
+# Description: The program "cpp.pl" functions as a compiler pre-processor (CPP) in the follopwing ways:
+#              it scans a text file indicated on the command line for special lines like the following
              
-             #define target1 replace1
-             #define target2 replace2
-             .
-             .
-             .
+               #define target1 replace1
+               #define target2 replace2
+               .
+               .
+               .
