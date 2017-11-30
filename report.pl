c149590066356e7f18ac0052aea49c1a4b4c3156
Pradeep Singh <pdeepsingh91@gmail.com>
2017-11-30 08:49:11 -0800

    Update report.pl

--- report.pl
+++ report.pl
@@ -3,58 +3,137 @@
 # File Name: report.pl
 # Author: Pradeep Singh
 # Date: 11/22/2017
-# Description: The program "report.pl" takes a filename and a sequence of
-#              strings through command line and the prints a summary of hits on
-#              string searches in the file.
+# Description: The program "report.pl" takes a file and sequence of strings through command line
+#              and then prints a summary of hits on string searches in the file.
 #
+#              Program begins with checking number of command line arguments; it gives usage information
+#              and terminates in case a usage problem is detected, following which user is prompted with
+#              error messgae.
 #
+#              First input argument is stored as "filename" and rest all are stored as "strings" to be searched
+#              in a array. Then, program attempts to open the file (given as first argument) for reading and
+#              reports and terminates if a problem is encountered.
 #
-# Program Usage:
-#              $ ./report.pl filename str1 [str2...]
+#              Then file is searched line by line using for-loop for given string, this is done for all the string.
+#              If string is matched in a line, then that line is stored along with line number and total number of matches.
 #
+#              Then, program prints a short report for each string passed through command line in following order:
+#              the string name, the number of lines that contain the string, and each line (with it's line numbner)
+#              containg the string.
 #
+#              Program is made executable using following command:
+#                           $ chmod u+x report.pl
 #
+#              Program Usage:
+#                           $ ./report.pl filename string1 string2 string3 ...
 #
+#===========================================================================
+# Program starts here
+#===========================================================================
 
-$newline="\n";
+# We begin with checking number of command line arguments.
+# And, print the total number of arguments.
 
-if ( @ARGV < 3 ) {
-  print $newline;
-  print("usage: ./report.pl filename str1 str2 ...");
-  print("usage: for example, ./report.pl programming perl fortran ...");
-  print $newline;
-  exit;
-}
+$newline= "\n";
 
-$filename = $ARGV[0];
-$string1  = $ARGV[1];
-$string2  = $ARGV[2];
-
+$numargs = @ARGV;      #Total input arguments
 print $newline;
-print(STDOUT "opening $filename... \n");
+print("Total number of arguments: $numargs\n");
 
-open(INFILE, $filename) or die "report.pl: $!";
+# Check the number of program command line arguments.
+# Give usage info and terminate in case a usage problem is detected.
 
-print(STDOUT "---------------------------------------------------\n");
-print(STDOUT "matches from perl search: \n");
-
-
-foreach $line[$i] ( <INFILE> ) {
-  chomp($line = $_);
-  @lines = split('\n',$filename);
-  $line[$i] = $line[Si] + 1;
+$newline= "\n";
+if ($numargs < 2) {     # if less than 2 arguments are passed, display this message
+   print $newline;
+   print("Program terminated. Usage problem is detected!\n");
+   print("Check the number of arguments and pass them in following manner.\n");
+   print("Usage: for example, ./report.pl filename string1 string2 string3 ...\n");
+   print $newline;
+   exit;
 }
 
-$icount = 0;
-$outstring = "";
+# Storing program command line arguments.
+# First input argument is taken as "filename" and remaining are taken as search strings.
 
-for ($i=0; $i < $numlines; $i++) {
-  $_ = $lines[$i];
-  if /$string1/ {
-    $blah = "line" .$i.":";
-    $outstring .=$blah.$_;
-    $icount++;
-  }
+$i = 0;                         # line counter
+my @strings_to_search = ();     # array to store string's
+
+foreach $argument (@ARGV) {
+   if ($i == 0){                # First input argument is filename
+      $filename = $argument;    # Stroing the filename
+      print("Filename: $filename \n");
+   }
+   else{                        # Remaining arguments are search strings
+      push @strings_to_search, $argument;       #Stroing in an array
+      print("Search string $i: $argument \n");
+   }
+
+   $i++;    # increment the counter
 }
+
+# Checking if entered file exists or not.
+open(INFILE, $filename) or die("\n Cannot open the input file $filename: $! \n\n");
+
+# Search the file for strings (line by line)
+
+my @data_file = ();             # array to store lines of files
+$curr_line_index = 0;           # variable to keep track of current a line in a file
+
+while (<INFILE>) {
+
+   $curr_line = $_;             # Stores current line
+   push @data_file, $curr_line; # Inserting at the end in the array
+
+# For loop to check whether this line contains the string or not.
+# If line contains the string, push the line number in string array, else
+# move to next line.
+
+   foreach $search_string (@strings_to_search){
+      if(index($curr_line, $search_string) != -1){
+         push @{$search_string}, $curr_line_index;
+      }
+   }
+$curr_line_index++;     # Incrementing line count
+}
+
+close(INFILE); # Close the file
+
+# -------------------------------------------------------------------
+# Printing the string search report in following format!
+# --------------------------------------------------------------------
+#
+#       Summary of hits on string searches in the file:
+#
+#       String Name:
+#       Number of lines that contain string
+#       Each line (with it's line number) containg string
+#
+#       **************************************************
+#       ........
+#       ........
+#       ........
+#
+#       **************************************************
+#       ........
+# --------------------------------------------------------------------
+
+print("\nSummary of hits on string searches in the file: \n");
+print(STDOUT "\n*********************************************************************************** \n");
+
+foreach $string (@strings_to_search){
+   print("Searched String: $string \n");    # print the string name
+   $no_of_matches = @{$string};         # total number of matches for string
+   print("Number of lines that contain string '$string' : $no_of_matches\n\n"); # print total number of matches for this string
+
+# Printing the lines that contains string
+   foreach $entry (@{$string}){
+      $line_to_print = $data_file[$entry];
+      print(STDOUT "line ${\($entry+1)}: $line_to_print");
+   }
+
+   print(STDOUT "\n************************************************************************************* \n");
+}
+
 
 
