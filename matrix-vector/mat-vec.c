21d50cd17cb30d0773aec0d46409fd569b0a07bf
Pradeep Singh <pdeepsingh91@gmail.com>
2018-02-19 17:16:30 -0800

    Update mat-vec.c

--- matrix-vector/mat-vec.c
+++ matrix-vector/mat-vec.c
@@ -32,21 +32,21 @@ int main()
    struct timeval start, end;
    gettimeofday(&start, NULL);                          /* start measuring time */
 
-/* Dynamic memory allocation for Matrices and Vector*/
-/* Allocate memory for matrix rows and columns = 1000 X 1000 */
+   /* Dynamic memory allocation for Matrices and Vector*/
+   /* Allocate memory for matrix rows and columns = 1000 X 1000 */
 
-/* Matrix -- 1 */
+   /* Matrix -- 1 */
    mat = (float **) malloc(1000 * sizeof(float *));        /* allocating memory to rows */
    for (i=0;i<1000;i++)                                    /* allocating memory to col */
        mat[i] = (float *) malloc(1000 * sizeof(float));
 
-/* Vector */
+   /* Vector */
    vec = (float *) malloc(1000 * sizeof(float *));         /* allocate memory for vector */
 
-/* Result Vector */
+   /* Result Vector */
    result = (float *) malloc(1000 * sizeof(float *));      /* allocate memory for result vector */
 
-/* Generating matrix elements with random numbers between 0 and 1 */
+   /* Generating matrix elements with random numbers between 0 and 1 */
    srand(time(NULL));                                           /* srand() sets the seed for rand() */
    for (i=0; i <1000; i++){
        vec [i] = (float) rand()/ (float) (RAND_MAX/a);          /* rand() generates the random number */
@@ -55,7 +55,7 @@ int main()
        }
    }
 
-/* Computing the Matrix - Vector product and storing it in result matrix */
+   /* Computing the Matrix - Vector product and storing it in result matrix */
    for (i = 0; i < 1000; i++) {
        result [i] = 0;
        for (j = 0; j < 1000; j++) {
@@ -65,10 +65,10 @@ int main()
 
   gettimeofday(&end, NULL);                             /* stop measuring time */
 
-/* Print the time elapsed */
+   /* Print the time elapsed */
    printf("\n Time Elapsed: %fs \n", ((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec)/1000000.0));
 
-/* Free the allocated memory for  matrices and vector using free() */
+   /* Free the allocated memory for  matrices and vector using free() */
 
    for (i = 0; i < 1000; i++){
        free(mat[i]);
